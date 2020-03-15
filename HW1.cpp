3a7f193f181702dab315e264077a0c80ab7a3430
sqree <49531694+sqree@users.noreply.github.com>
2020-03-15 10:48:35 -0500

    Update HW1.cpp

--- HW1.cpp
+++ HW1.cpp
@@ -51,7 +51,7 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 04 - Colored Cube", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "HW 1 - bouncing ball", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
